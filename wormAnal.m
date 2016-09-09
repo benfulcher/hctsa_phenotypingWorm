f23fec5e37b73f098617dd59c1aa539ffb369083
Ben Fulcher <ben.d.fulcher@gmail.com>
2016-09-09 13:31:38 +1000

    Removed redundant sections

--- wormAnal.m
+++ wormAnal.m
@@ -42,19 +42,5 @@ TS_cluster('none',[],'corr','average',[0,1],normalizedFileName);
 TS_plot_DataMatrix('cl','colorGroups',1,'groupReorder',1)
 
 %-------------------------------------------------------------------------------
-% Top feature space?:
-TS_ForwardFS(filteredData,[],'diaglinear',5,2)
-
-%-------------------------------------------------------------------------------
-% [~,restrictedFileName] = TS_LabelGroups(theGroups,'raw',1,1);
-% normalizedFileName = TS_normalize('none',[0.5,1],restrictedFileName);
-% TS_TopFeatures('HCTSA_filtered_N.mat','fast_linear',0)
-
-% %-------------------------------------------------------------------------------
-% % Forward feature selection:
-% [TS_DataMat,TimeSeries] = TS_LoadData('HCTSA_notLocDep_filtered_N.mat');
-% fn_testStat = @(XTrain,yTrain,Xtest,yTest) sum(yTest ~= classify(Xtest,XTrain,yTrain,'diaglinear'));
-% opts = statset('display','iter');
-% [fs,history] = sequentialfs(fn_testStat,TS_DataMat,[TimeSeries.Group]',...
-%                     'cv',5,'options',opts);
-% fprintf(1,'Selected %u features\n',sum(fs));
+% Top feature space? (slow):
+TS_ForwardFS(normalizedData,[],'fast_linear',5,2);
